4dbd63173cda6e65f886832e33d6a8dbb598c1d9
Numidian-Dev <numidiandev@gmail.com>
2026-08-08 21:22:55 +0100

    maj 8/8/21:22

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import logo from "../../assets/logos/Logo.png"
 import logoTxt from "../../assets/logos/logo-text.svg"
-import { FaLink } from "react-icons/fa6";
+import { FaLink, FaInstagram,FaXTwitter,FaTiktok, FaYoutube } from "react-icons/fa6";
+
+
 
 export default function Header() {
   return (
@@ -15,6 +17,16 @@ export default function Header() {
         <p className="description">Des expériences uniques. Par un studio indépendant 100 % créatif 🐝</p>
     
         <a href="hornetsoftent.com" className="link"><FaLink className='icon'/>hornetsoftent.com</a>
+        <div className="icons-social">
+        <a target='_blank' href="https://www.instagram.com/hornetsoftent/"><FaInstagram className='social-icon'/></a>
+        <a target='_blank' href="https://x.com/HornetSoftent"><FaXTwitter className='social-icon'/></a>
+        <a target='_blank' href="https://www.tiktok.com/@hornetsoftent_"><FaTiktok className='social-icon'/></a>
+        <a target='_blank' href="https://www.youtube.com/@HornetSoftent"><FaYoutube className='social-icon'/></a>
+          
+          
+          
+          
+        </div>
     </header>
   )
 }
